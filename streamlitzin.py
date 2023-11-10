ef85851fbc6d8d28d67d375524fdc80fabbff182
AleArgentino <141688912+AleArgentino@users.noreply.github.com>
2023-11-09 21:29:27 -0300

    Update streamlitzin.py

--- streamlitzin.py
+++ streamlitzin.py
@@ -22,70 +22,12 @@ dataD = rD.content
 df = pd.read_csv(BytesIO(dataD))
 df
 
+#Gráfico de uma lavoura específico
+lav_escolhida = df[Nome Lavoura] == st.selectbox("Escolha um produto",["Guaraná", "Cana-de-açucar", "Soja", "café", "Goiaba"])
+dfLav = df[lav_escolhida]
+dfLav
+
 #Gráfico de um ano específico
 ano_escolhido = df['Ano'] == int(st.slider("Escolha um ano que deseja se aprofundar:", 1990, 2021))
 dfAno = df[ano_escolhido]
 dfAno
-
-#Seleção da lavoura
-sele_G = dfAno['Nome Lavoura']=="Guaraná"
-dfG = dfAno[sele_G]
-
-sele_C = dfAno['Nome Lavoura']=="Cana-de-açucar"
-dfC = dfAno[sele_C]
-
-sele_S = dfAno['Nome Lavoura']=="Soja"
-dfS = dfAno[sele_S]
-
-sele_Ce = dfAno['Nome Lavoura']=="Café"
-dfCe = dfAno[sele_Ce]
-
-sele_Go = dfAno['Nome Lavoura']=="Goiaba"
-dfGo = dfAno[sele_Go]
-
-#Gráfico de Área
-fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
-
-labels = 'Cana-de-Açucar', 'Guaraná', 'Soja', 'Café', 'Goiaba'
-data = [float(dfC['Área Colhida (em milhares de hectares)']), float(dfG['Área Colhida (em milhares de hectares)']), float(dfS['Área Colhida (em milhares de hectares)']), float(dfCe['Área Colhida (em milhares de hectares)']), float(dfGo['Área Colhida (em milhares de hectares)'])]
-explode = (0.1, 0.1, 0.1, 0.1, 0.1)
-
-def func(pct, allvals):
-    absolute = int(np.round(pct/100.*np.sum(allvals)))
-    return f"{pct:.1f}%\n({absolute:d} ha*10³)"
-
-wedges, texts, autotexts = ax.pie(data, explode = explode, autopct=lambda pct: func(pct, data),
-                                  textprops=dict(color="black"))
-
-ax.legend(wedges, labels,
-          title="Produtos",
-          loc="center left",
-          bbox_to_anchor=(1, 0, 0.5, 1))
-
-plt.setp(autotexts, size=8, weight="bold")
-
-ax.set_title("Percentual de Área Colhida")
-
-Atotal = float(dfG['Área Colhida (em milhares de hectares)']) + float(dfC['Área Colhida (em milhares de hectares)']) + float(dfS['Área Colhida (em milhares de hectares)']) + float(dfCe['Área Colhida (em milhares de hectares)']) + float(dfGo['Área Colhida (em milhares de hectares)'])
-
-print("A área total utilizada no ano escolhido: " + str(Atotal) + " milhares de hectares.")
-print()
-
-plt.show()
-print()
-
-fig, ax = plt.subplots()
-
-labels = 'Cana-de-Açucar', 'Guaraná', 'Soja', 'Café', 'Goiaba'
-data = [float(dfC['Área Colhida (em milhares de hectares)']), float(dfG['Área Colhida (em milhares de hectares)']), float(dfS['Área Colhida (em milhares de hectares)']), float(dfCe['Área Colhida (em milhares de hectares)']), float(dfGo['Área Colhida (em milhares de hectares)'])]
-
-bar_labels = ['blue', 'orange', 'green', 'brown', 'red']
-bar_colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:brown', 'tab:red']
-
-ax.bar(labels, data, label=labels, color=bar_colors)
-
-ax.set_ylabel('Milhares de Hectares')
-ax.set_title('Área Colhida')
-ax.legend(title='Produtos')
-
-plt.show()
