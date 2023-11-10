c2d02f8c7eb48e9ab14eaa85d5d9d35f685aae8f
AleArgentino <141688912+AleArgentino@users.noreply.github.com>
2023-11-09 21:03:11 -0300

    Update streamlitzin.py

--- streamlitzin.py
+++ streamlitzin.py
@@ -1,5 +1,3 @@
-pip install matplotlib
-
 #Libraries
 import streamlit as st
 import pandas as pd
@@ -7,7 +5,14 @@ import urllib3
 from urllib3 import request
 from io import BytesIO
 import requests
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 
 st.title("Trabalho Fina de Análise de Dados")
+
+#Gráfico Base
+http = urllib3.PoolManager()
+rD = requests.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vSdXh5LgfEPrWYfXBlR-qNK25CmO-VgIHO1h_CEjChC1eMHl5z5ds3UPjIo5TNb34EQIFrK-bUICDev/pub?gid=0&single=true&output=csv')
+dataD = rD.content
+df = pd.read_csv(BytesIO(dataD))
+df
